f6ec6d8c50a9ebbead4ddc47c1afa20e8cac7719
halukijl <halukijl@users.noreply.github.com>
2020-01-28 11:12:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ while menuOption != '6':
     #print(menuOption)
     if menuOption == '1':
         print("Add item.")
+        item = input("What item would you like to add:  ")
+        mylist.append(item)
+        print (mylist)
     elif menuOption == '2':
         print('Print list')
         mylist.sort(key=str.lower)
@@ -26,10 +29,32 @@ while menuOption != '6':
             i += 1
     elif menuOption == '3':
         print("Remove item by number.")
+        print('What item would you like to remove: ')
+        number = input()
+        try:
+            number = int(number)
+            myListTemp = []
+            removedItem = mylistmylist[number-1]
+            for item in mylist:
+                if item != removedItem:
+                    myListTemp.append(item)
+            mylist = myListTemp
+        except Exception as e:
+            print('Not a valid input' + str(e))
     elif menuOption == '4':
         print("Save list to file.")
+        file = open('shoppingList.txt', 'w')
+        i = 1
+        for item in mylist:
+            file.write(str(i) + '). ' + item + '\n')
+            i += 1
+        file.close()
     elif menuOption == '5':
         print("Load list from file.")
+        file = open('shoppingList.txt', 'r')
+        mylist = []
+        for line in file:
+            mylist.append(line[4:-1])
     elif menuOption == '6':
         print("Exit.")
     else:
